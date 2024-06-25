a4d3227af7ee7f580c27901e804edd5ee9356db6
Leonardotorre18 <Leonardotorre14@gmail.com>
2024-06-25 12:34:15 -0400

    Correción de código

--- script.js
+++ script.js
@@ -42,10 +42,29 @@ axios.get('https://leonardoapi.onrender.com/songs')
     .then((res) => {
 
         const songList = res.data.songs
+        const trackList = document.getElementById('track-list')
 
         songList.map((song) => {
 
-            console.log(song)  
+            const div = document.createElement('div')
+            div.classList.add('clase-para-canciones')
+
+            div.innerHTML = `
+                <img src="${song.path.front}" alt="">
+                <p>${song.author} - ${song.title}</p>
+                <button>
+                    <img src="/Assets/multimedia_pause_button_icon_149073 1.png" alt="">
+                </button>
+                <button>
+                    <img src="/Assets/play.png" alt="">
+                </button>
+                <button>
+                    <img src="/Assets/dots_three_outline_vertical_thin_icon_171812 1.png" alt="">
+                </button>
+            `
+
+
+            trackList.appendChild(div) 
         })
 
     })
